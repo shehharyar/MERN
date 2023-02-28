c13b5a1c2a864ab5b407eb77381ef9a025e54d2f
shehharyar <sheharyarhassan1456@gmail.com>
2023-02-28 23:10:41 +0500

    demo mongodb practice

---
+++ Backend/playground/mongo.js
@@ -0,0 +1,27 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url ="mongodb+srv://Sheharyar:ydzmUCVEBQ1HHTAp@cluster0.vosumum.mongodb.net/?retryWrites=true&w=majority"
+
+const createProduct = async (req, res, next) => {
+  const newProduct = {
+    name: req.body.name,
+    price: req.body.price
+  };
+  const client = new MongoClient(url);
+
+  try {
+    await client.connect();
+    const db = client.db();
+    const result = db.collection('products').insertOne(newProduct);
+  } catch (error) {
+    return res.json({message: 'Could not store data.'});
+  };
+  client.close();
+
+  res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {};
+
+exports.createProduct = createProduct;
+exports.getProducts = getProducts;
