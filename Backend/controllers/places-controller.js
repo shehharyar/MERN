3ebb0be7d9dd244f7791016658d166dab7b5d4f0
shehharyar <sheharyarhassan1456@gmail.com>
2023-03-11 23:50:49 +0500

    added request for get place by id from database

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -16,23 +16,25 @@ const Place= require("../models/place");
 //     }
 //   ];
 
-const getPlacesById= (req, res, next) => {
+   const getPlacesById = async(req, res, next) => {
     const placeId = req.params.pid; // { pid: 'p1' }
-  
-    const place = DUMMY_PLACES.find(p => {
-      return p.id === placeId;
-    });
+    let place;
+    try {
+       place = await Place.findById(placeId);
+    } catch (err) {
+      const error= new HttpError("Something went wrong. Could not find a place", 500) 
+      return next(error);
+    }
   
     if (!place) {
       throw new HttpError('Could not find a place for the provided id.', 404);
     }
   
-    res.json({ place }); // => { place } => { place: place }
+    res.json({ place: place.toObject({ geetters: true}) }); // => { place } => { place: place }
   };
 
   const getPlacesByUserId=(req, res, next) => {
     const userId = req.params.uid;
-  
     const places = DUMMY_PLACES.filter(p => {
       return p.creator === userId;
     });
