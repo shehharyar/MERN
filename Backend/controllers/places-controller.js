b4de4c4c1c7b89dc17af1f699cf6f2cbbd3839c5
shehharyar <sheharyarhassan1456@gmail.com>
2023-03-19 18:30:02 +0500

    Added delete query to delete data using mongoose

--- Backend/controllers/places-controller.js
+++ Backend/controllers/places-controller.js
@@ -114,17 +114,34 @@ try {
 
     res.status(200).json({place: place.toObject({getters: true}) });
   }
-  const deletePlaceById= ((req, res,next)=>{
+  
+  const deletePlaceById= async (req, res,next)=>{
     const placeId= req.params.pid;
 
-    if(!DUMMY_PLACES.find(p => p.id === placeId)){
-      throw new HttpError("Could not find a place for that id", 404);
+    let place;
+    try{
+      place= await Place.findByIdAndDelete(placeId);
+    }catch(e){
+      const error= new HttpError("Something Wrong. Could not delete plcae", 500);
+      return next(error);
     }
 
-    DUMMY_PLACES= DUMMY_PLACES.filter(p => p.id !== placeId);
+    // try{
+    //   await place.remove();
+    // }catch(e){
+    //   const error= new HttpError("Something Wrong. Could not delete plcae", 500);
+    //   return next(error);
+    // }
+
+    
+    // if(!DUMMY_PLACES.find(p => p.id === placeId)){
+    //   throw new HttpError("Could not find a place for that id", 404);
+    // }
+
+    // DUMMY_PLACES= DUMMY_PLACES.filter(p => p.id !== placeId);
     
     res.status(200).json({message : "Place Deleted"});
-  });
+  };
 
   exports.getPlacesById= getPlacesById;
   exports.getPlacesByUserId= getPlacesByUserId;
