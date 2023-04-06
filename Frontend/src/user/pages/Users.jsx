2fdc7539f8c1c2590f4e6ca8e9ef8fe4aa081f79
shehharyar <sheharyarhassan1456@gmail.com>
2023-04-06 23:55:26 +0500

    sent a get request with fetch api

--- Frontend/src/user/pages/Users.jsx
+++ Frontend/src/user/pages/Users.jsx
@@ -1,19 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import UsersList from '../components/UsersList';
+import UsersList from '../components/UsersList.jsx';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal.jsx';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner.jsx';
 
 const Users = () => {
-  const USERS = [
-    {
-      id: 'u1',
-      name: 'Max Schwarz',
-      image:
-        'https://images.pexels.com/photos/839011/pexels-photo-839011.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-      places: 3
-    }
-  ];
+  const[isLoading, setIsLoading]= useState(false);
+  const[loadedUsers, setLoadedUsers]= useState();
+  const[error, setError]= useState();
 
-  return <UsersList items={USERS} />;
+  useEffect(() => {
+    const sendRRequest= async () => {
+      setIsLoading(true);
+        try {
+          const response= await fetch("http://localhost:5000/api/users");
+          const responseData= await response.json();
+          if(!response.ok){
+            throw new Error(responseData.message);
+          }
+
+          setLoadedUsers(responseData.users);
+        } catch (error) {
+          setError(err.message);
+        }
+        setIsLoading(false);
+
+    };
+
+    sendRRequest();
+  }, [])
+  
+  const errorHandler= ()=>{
+    setError(null);
+  }
+
+  return 
+  <>
+  {error && <ErrorModal error={error} onClear= {errorHandler}/> }
+  {
+    isLoading &&
+    (
+      <div className="center">
+        <LoadingSpinner/>
+
+      </div>)
+
+}
+{!isLoading && loadedUsers && <UsersList items={loadedUsers.users} /> }
+
+
+
+  </>
 };
 
 export default Users;
